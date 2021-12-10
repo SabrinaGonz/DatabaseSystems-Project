78822a777fc76214eb7220ce753ca7c71420e5b5
JudeOrsua <70415065+JudeOrsua@users.noreply.github.com>
2021-12-09 17:33:39 -0800

    Add files via upload

--- Implementation.py
+++ Implementation.py
@@ -1,5 +1,6 @@
 import sqlite3
 from sqlite3 import Error
+import random
 
 def openConnection(dbfile):
     print("Opening Database: ", dbfile)
@@ -48,6 +49,12 @@ def dropTables(conn):
         sql = """drop table if exists ReleaseDate"""
         conn.execute(sql)
 
+        sql = """drop view if exists movies"""
+        conn.execute(sql)
+
+        sql = """drop view if exists shows"""
+        conn.execute(sql)
+
         conn.commit()
         print("success")
     except Error as e:
@@ -87,8 +94,9 @@ def createTables(conn):
         conn.execute(sql)
 
         sql = """create table Favorite (
-                f_sid           decimal(9,0) not null,
-                f_like          char(117) not null)"""
+                f_sid            varchar(10) not null,
+                f_title          char(117))"""
+        conn.execute(sql)
 
         conn.commit()
         print("success")
@@ -144,17 +152,197 @@ def choices(conn):
     selection = int(input())
     return selection
 
-def search(conn):
-    print("you have selected option 1")
-
 def favorites(conn):
-    print("you have selected option 2")
+    print("Here is your current Favorite list")
+
+    sql = "select * from Favorite"
+    cur = conn.cursor()
+    cur.execute(sql)
+    l = '{:>10} {:>10}'.format("Show ID", "Title")
+    print(l + "\n")
+
+    rows = cur.fetchall()
+    for row in rows:
+        l = '{:>10} {:>10}'.format(row[0], row[1])
+        print(l)
+    print("\n")
+
+def addfavorites(conn):
+    s = input("Please enter the Show ID of the title you wish to add to your Favorites list: ")
+    try:
+        sql = """insert into Favorite(f_sid, f_title)
+                select t_sid, t_title
+                from Titles 
+                where t_sid = ?"""
+        conn.execute(sql, (s,))
+        conn.commit()
+        favorites(conn)
+    except Error as e:
+        print("Sorry, we were not able to add that show into your Favorites list.\nPlease double check that you also included 's' into the Show ID.\n")
+        addfavorites(conn)
+
+def search(conn):
+    cur = conn.cursor()
+    s = '%'
+    s += input("Please enter what title you are searching for: ")
+    s += '%'
+    with conn:
+        sql = """select t_sid, t_title
+                from Titles
+                where t_title like ?"""
+        cur.execute(sql, (s,))
+
+        l = '{:>10} {:>10}'.format("Show ID", "Title")
+        print(l + "\n")
+
+        rows = cur.fetchall()
+        for row in rows:
+            l = '{:>10} {:>10}'.format(row[0], row[1])
+            print(l)
+    print("If the title you are looking for does not appear, it is not available on Netflix.\n")
+
+    fav = input("If you would like to add a title to your favorites, please enter 1 or 0 if not: ")
+    if fav == "1":
+        addfavorites(conn)
+    
+    print("Okay, returning you to the main menu.\n\n")
+    
+
+def randomshow(conn):
+    print("Here is a random show in Netflix: \n")
+    r = "s"
+    r += str(random.randint(1, 8807))
+    cur = conn.cursor()
+    sql = """select t_sid, t_title, t_type
+            from Titles
+            where ? = t_sid"""
+    cur.execute(sql, (r,))
+
+    l = '{:>10} {:>10} {:>30}'.format("Show ID", "Title", "Show Type")
+    print(l + "\n")
+
+    rows = cur.fetchall()
+    for row in rows:
+        l = '{:>10} {:>10} {:>30}'.format(row[0], row[1], row [2])
+        print(l + "\n")
+    
+
+def typefilter(conn):
+    print("Would you like: ")
+    print("     1. Movies")
+    print("     2. TV Shows")
+    i = int(input())
+    return i
+
+def morefilter(conn):
+    m = int(input("If you would like to filter more, please enter 1 or 0 if not: "))
+    return m
+    
+def genrefilter(conn):
+    print("Would you like: ")
+    print("     1. Action & Adventure")
+    print("     2. Anime")
+    print("     3. Children & Family")
+    print("     4. Comedies")
+    print("     5. Horror")
+    print("     6. Romantic")
+    print("     7. Thrillers")
+    g = int(input())
+    return g
 
 def filter(conn):
-    print("you have selected option 3")
+    sql = """drop view if exists movies"""
+    conn.execute(sql)
+    sql = """drop view if exists shows"""
+    conn.execute(sql)
+    sql = """drop view if exists actionadventuremovies"""
+    conn.execute(sql)
+    sql = """drop view if exists actionadventureshows"""
+    conn.execute(sql)
+    conn.commit()
 
-def random(conn):
-    print("you have selected option 4")
+    cur = conn.cursor()
+    t = typefilter(conn)
+    if t == 1:
+        sql = """create view movies(sid, title, genre, release, rating) as 
+                select n_sid, n_title, n_listed, n_release, n_rating
+                from Netflix
+                where n_type = 'Movie'"""
+        cur.execute(sql)
+        conn.commit()
+        sql = "select * from movies"
+        cur.execute(sql)
+
+        l = '{:>10} {:>20} {:>30} {:>20} {:>20}'.format("Show ID", "Title", "Genre", "Release Date", "Rating")
+        print(l)
+
+        rows = cur.fetchall()
+        for row in rows:
+            l = '{:>10} {:>20} {:>30} {:>20} {:>20}'.format(row[0], row[1], row[2], row[3], row[4])
+            print(l)
+    else:
+        sql = """create view shows(sid, title, genre, release, rating) as 
+                select n_sid, n_title, n_listed, n_release, n_rating
+                from Netflix
+                where n_type = 'TV Show'"""
+        cur.execute(sql)
+        conn.commit()
+        sql = "select * from shows"
+        cur.execute(sql)
+        l = '{:>10} {:>20} {:>30} {:>20} {:>20}'.format("Show ID", "Title", "Genre", "Release Date", "Rating")
+        print(l)
+
+        rows = cur.fetchall()
+        for row in rows:
+            l = '{:>10} {:>20} {:>30} {:>20} {:>20}'.format(row[0], row[1], row[2], row[3], row[4])
+            print(l)
+
+    # print()
+    # g = genrefilter(conn)
+    # if g == 1 & t == 1:             
+    #     sql = """create view actionadventuremovies(sid, title, genre, release, rating) as 
+    #             select n_sid, n_title, n_listed, n_release, n_rating
+    #             from Netflix
+    #             where n_listed = 'Action & Adventure'
+    #             and n_type = 'Movie'"""
+    #     cur.execute(sql)
+    #     conn.commit()
+    #     sql = "select * from actionadventuremovies"
+    #     cur.execute(sql)
+        
+    #     l = '{:>10} {:>20} {:>30} {:>20} {:>20}'.format("Show ID", "Title", "Genre", "Release Date", "Rating")
+    #     print(l)
+
+    #     rows = cur.fetchall()
+    #     for row in rows:
+    #         l = '{:>10} {:>20} {:>30} {:>20} {:>20}'.format(row[0], row[1], row[2], row[3], row[4])
+    #         print(l)
+            
+    # elif g == 1 & t == 2:
+    #     sql = """create view actionadventureshows(sid, title, genre, release, rating) as 
+    #             select n_sid, n_title, n_listed, n_release, n_rating
+    #             from Netflix
+    #             where n_listed like '%Action%'
+    #             and n_type = 'TV Show'"""
+    #     cur.execute(sql)
+    #     conn.commit()
+    #     sql = "select * from actionadventureshows"
+    #     cur.execute(sql)
+        
+    #     l = '{:>10} {:>20} {:>30} {:>20} {:>20}'.format("Show ID", "Title", "Genre", "Release Date", "Rating")
+    #     print(l)
+
+    #     rows = cur.fetchall()
+    #     for row in rows:
+    #         l = '{:>10} {:>20} {:>30} {:>20} {:>20}'.format(row[0], row[1], row[2], row[3], row[4])
+    #         print(l)
+
+    
+        
+            
+
+
+
 
 
 def main():
@@ -184,7 +372,7 @@ def main():
             elif i == 3:
                 filter(conn)
             elif i == 4:
-                random(conn)
+                randomshow(conn)
 
     closeConnection(conn, database)
 
